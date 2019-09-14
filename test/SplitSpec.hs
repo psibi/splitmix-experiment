bf296f6d698e264083588ad2fade7fb447761597
Sibi Prabakaran <sibi@psibi.in>
2019-09-14 12:03:04 +0530

    Update test

--- test/SplitSpec.hs
+++ test/SplitSpec.hs
@@ -1,7 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module SplitSpec where
 
+import Data.Text (Text, pack)
+import System.Random (randomR)
 import System.Random.SplitMix
 import Test.Hspec
+
+populateRandomString :: SMGen -> Text
+populateRandomString smGen = helper smGen 4 []
+  where
+    helper :: SMGen -> Int -> [Char] -> Text
+    helper gen counter acc =
+      if counter == 0
+        then pack acc
+        else do
+          let (char, ngen) = randomR ('A', 'Z') gen
+          helper ngen (counter - 1) (acc ++ [char])
 
 spec :: Spec
 spec = do
@@ -11,3 +26,7 @@ spec = do
           gen = seedSMGen' seed
           outputSeed = unseedSMGen gen
       seed `shouldBe` outputSeed
+    it "random string" $ do
+      let seed = (10000, 10000)
+      let txt = populateRandomString (seedSMGen' seed)
+      txt `shouldBe` "QHUH"
