78a9ae5be8e512444b995d129b459e62105aa70b
Sibi Prabakaran <sibi@psibi.in>
2019-09-15 11:46:34 +0530

    Update test

--- test/SplitSpec.hs
+++ test/SplitSpec.hs
@@ -16,13 +16,8 @@ populateRandomString smGen =
 spec :: Spec
 spec = do
   describe "Splitmax test" $ do
-    it "seed/unseed" $ do
-      let seed = (10000, 10000)
-          gen = seedSMGen' seed
-          outputSeed = unseedSMGen gen
-      seed `shouldBe` outputSeed
     it "random string" $ do
-      let seed = (10000, 10000)
+      let seed = (10000, 10001)
       let txt = populateRandomString (seedSMGen' seed)
       txt `shouldBe` "QH"
     it "splitSMGen" $ do
